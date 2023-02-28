a23491d5f485ac9fe69f989fbc0c56524988c577
Mohamed Mehdawy <mohammedmehdawy@gmail.com>
2023-02-28 16:16:59 +0200

    build animation for clouds

--- src/script/main.ts
+++ src/script/main.ts
@@ -1,4 +1,25 @@
 // style
 import "../style/main.scss";
+import { gsap } from "gsap";
+import {ScrollTrigger} from "gsap/ScrollTrigger";
 
-// gsap
+// register scroll trigger
+gsap.registerPlugin(ScrollTrigger)
+
+// animation
+const header = document.querySelector("#main-header") as HTMLElement; 
+const tl = gsap.timeline({
+    scrollTrigger: {
+        trigger: header,
+        markers: true,
+        start: "top",
+        end: "bottom",
+        scrub: 2,
+    }
+}).to(["#cloud1", "#cloud4"], {
+    x: 1000,
+    y: 100
+}).to(["#cloud2", "#cloud3"], {
+    x: -1000,
+    y: 100
+}, 0)
